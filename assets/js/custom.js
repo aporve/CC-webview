a7ea0e025b4f47a3ec7d9b5578356ca1a2fe997d
aporve <aporvep@gmail.com>
2022-04-12 11:40:53 +0530

    update the code

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,10 +3,20 @@ window.addEventListener('message', function (eventData) {
     console.log(eventData);
     try {
         if (JSON.parse(eventData.data)) {
-            let event = JSON.parse(eventData.data);
-            console.log(event, 'Event~~~~~~~~~>>')
-            console.log(event.data.code, 'Coke Event~~~~~~>>')
-            console.log(event.data.data, 'coke Data~~~~~~~~~~>>')
+            let eventData = JSON.parse(eventData.data);
+            console.log('Event~~~~~~~~~>>',event)
+            console.log('Coke Event~~~~~~>>',event.data.code)
+            console.log('coke Data~~~~~~~~~~>>',event.data.data)
+            let event = event.data.code; 
+            let eventParseData = event.data.data;
+            console.log(event, 'Event~~~~>>')
+            console.log(eventParseData, 'Event parse data~~~~~~~~>>');
+
+            if(event == 'coke-label') {
+                config = event.data.data
+                console.error(config, 'Config~~~~>>')
+            }
+
             // if (event.event_code === 'coke-label') {
             //     console.log(event.event_code, 'Event code~~~~~~~~~~>>')
             //     var data = event.event_code;
